de5dac02d8f45d734fd691b35f8a063e0a42e04e
Mark Hernandez <markkimhernandez@gmail.com>
2020-07-21 19:07:03 -0500

    Implement Drawer-Based Navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,11 +1,11 @@
 //This component will be the main point of control for the state 
 import React, { Component } from 'react';
-
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import { ENTREES } from '../shared/mainEntrees';
 import MainEntreeInfo from './MainEntreesComponent';
+import { ENTREES } from '../shared/mainEntrees';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -26,6 +26,33 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -43,7 +70,7 @@ class Main extends Component {
     render() {
         return(
             <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <DirectoryNavigator />
+                <MainNavigator />
             </View>
             
         );
