ee9c6224a47059591717a542c41d968e126ff4b5
Mark Hernandez <markkimhernandez@gmail.com>
2020-07-24 23:10:31 -0500

    Updated About Component with Mission, Catering, Giving Back Information

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,5 +1,47 @@
 import React,  { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+function Mission() {
+
+    return(
+        <Card title='About Us'>
+            <Text>
+                For over 25 years Heart &amp; Seoul has been the premiere location for fine Korean cuisine.
+                From the start we make sure our customers receive nothing but the best in
+                quality. That means we use the freshest and finest ingredients to prepare your food. Our staff 
+                works hard to make sure you are treated like royalty. They take pride in delivering quality service
+                to meet your needs. Please join us for your next adventure into Korean cuisine. 
+            </Text>
+        </Card>
+    );
+}
+
+function Catering() {
+
+    return(
+        <Card title='Catering'>
+            <Text>
+                Heart &amp; Seoul also caters for special occasions. We can create the right package for you.
+                Need to feed your employees? How about a wedding or baby shower? Call us for more details.
+            </Text>
+        </Card>
+    );
+}
+
+function GivingBack() {
+
+    return(
+        <Card title='Giving Back'>
+            <Text>
+                For 20 years we have given back to the community that has given so much to us. This is our way
+                of saying thank you and appreciating the love and support from you. Every year
+                we fight to end hunger where we live. Please join us in that fight.
+            </Text>
+        </Card>
+    );
+    
+}
 
 class About extends Component {
 
@@ -14,7 +56,9 @@ class About extends Component {
     render() {
         return(
             <ScrollView>
-                
+                <Mission />
+                <Catering />
+                <GivingBack />
             </ScrollView>
         );
     }
